3a230188509433cf889b108a5d2c8c8104601dcc
Hélder Lima <orfeowrk@gmail.com>
2023-11-08 21:32:20 -0300

    scanner

--- src/vm.c
+++ src/vm.c
@@ -2,6 +2,7 @@
 
 #include "chunk.h"
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "value.h"
 #include "vm.h"
@@ -93,9 +94,8 @@ static InterpretResult run() {
 
 }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char* source) {
+  compile(source);
+  return INTERPRET_OK; 
 }
 
